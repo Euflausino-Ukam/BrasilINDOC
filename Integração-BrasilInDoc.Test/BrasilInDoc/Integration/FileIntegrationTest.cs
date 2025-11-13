23472e7bae19d5635ed6a01abafbc08831e73fb4
Euflausino-Ukam <eduardo.euflausino@ukam.com.br>
2025-11-13 15:52:26 -0300

    2025-11-13

--- Integração-BrasilInDoc.Test/BrasilInDoc/Integration/FileIntegrationTest.cs
+++ Integração-BrasilInDoc.Test/BrasilInDoc/Integration/FileIntegrationTest.cs
@@ -1,108 +1,108 @@
-﻿//using Bogus;
-//using Errors;
-//using Integração_BrasilInDoc.BrasilInDoc.Infrastructure.Integration;
-//using Shouldly;
-//using System.Net;
-//using System.Text;
+﻿using Bogus;
+using Errors;
+using Integração_BrasilInDoc.BrasilInDoc.Infrastructure.Integration;
+using Shouldly;
+using System.Net;
+using System.Text;
 
-//namespace Integração_BrasilInDoc.Test.BrasilInDoc.Integration
-//{
-//    [TestClass]
-//    [TestCategory("File Integration Test")]
-//    public class FileIntegrationTest
-//    {
-//        private readonly static Faker _faker = new("pt_BR");
-//        private readonly string apiKey = "apiKeyTest";
+namespace Integração_BrasilInDoc.Test.BrasilInDoc.Integration
+{
+    [TestClass]
+    [TestCategory("File Integration Test")]
+    public class FileIntegrationTest
+    {
+        private readonly static Faker _faker = new("pt_BR");
+        private readonly string apiKey = "apiKeyTest";
 
-//        [TestMethod]
-//        [DynamicData(nameof(FileNotFoundErrorToSend))]
-//        public async Task Upload_File_Async_FileNotFound(string filePathInvalid)
-//        {
-//            // Arrange
-//            var receiver = _faker.Person.FirstName;
+        [TestMethod]
+        [DynamicData(nameof(FileNotFoundErrorToSend))]
+        public async Task Upload_File_Async_FileNotFound(string filePathInvalid)
+        {
+            // Arrange
+            var receiver = _faker.Person.FirstName;
 
-//            var service = new FileIntegration(new HttpClient());
+            var service = new FileIntegration(new HttpClient());
 
-//            // Act
-//            var expectedResult = await Assert.ThrowsAsync<ErrorLists>(async () =>
-//            {
-//                await service.UploadFileAsync(apiKey, filePathInvalid, receiver);
-//            });
+            // Act
+            var expectedResult = await Assert.ThrowsAsync<ErrorLists>(async () =>
+            {
+                await service.UploadFileAsync(apiKey, filePathInvalid, receiver);
+            });
 
-//            // Assert
-//            if (filePathInvalid == "" || expectedResult.Errors.Contains("Diretório")) 
-//                expectedResult.Errors.ShouldContain($"Diretório não encontrado ao tentar acessar o arquivo: {filePathInvalid}");
+            // Assert
+            if (filePathInvalid == "" || expectedResult.Errors.Contains("Diretório"))
+                expectedResult.Errors.ShouldContain($"Diretório não encontrado ao tentar acessar o arquivo: {filePathInvalid}");
 
-//            if (filePathInvalid is null)
-//                expectedResult.Errors.ShouldContain("Caminho para o arquivo nulo");
-            
-//            if (expectedResult.Errors.Contains("Arquivo não encontrado"))
-//                expectedResult.Errors.ShouldContain($"Arquivo não encontrado: {Path.GetFileName(filePathInvalid)}");
+            if (filePathInvalid is null)
+                expectedResult.Errors.ShouldContain("Caminho para o arquivo nulo");
 
-//        }
-//        public static IEnumerable<object[]> FileNotFoundErrorToSend()
-//        {
-//            var filePathInvalid = Path.Combine("C:\\Arquivo\\", $"{Guid.NewGuid()}.txt");
-//            var fileNotFound = Path.Combine("C:\\Users\\Ajinsoft\\Pictures\\imgs\\", $"{_faker.Name.FirstName()}.jpg");
+            if (expectedResult.Errors.Contains("Arquivo não encontrado"))
+                expectedResult.Errors.ShouldContain($"Arquivo não encontrado: {Path.GetFileName(filePathInvalid)}");
 
-//            yield return new object[] { "" };
-//            yield return new object[] { null };
-//            yield return new object[] { filePathInvalid };
-//            yield return new object[] { fileNotFound };
-//        }
+        }
+        public static IEnumerable<object[]> FileNotFoundErrorToSend()
+        {
+            var filePathInvalid = Path.Combine("C:\\Arquivo\\", $"{Guid.NewGuid()}.txt");
+            var fileNotFound = Path.Combine("C:\\Users\\Ajinsoft\\Pictures\\imgs\\", $"{_faker.Name.FirstName()}.jpg");
 
-//        [TestMethod]
-//        [DynamicData(nameof(FileToSent))]
-//        public async Task Upload_File_Async_HttpException(string filePathValid, string receiver)
-//        {
-//            // Arrange
-//            var exception = new HttpRequestException("Falha na comunicação");
-//            var requestMock = Helpes.Handlers.HttpHelperMock.CreateHttpClient
-//                (out var handlerMock, ex: exception);
+            yield return new object[] { "" };
+            yield return new object[] { null };
+            yield return new object[] { filePathInvalid };
+            yield return new object[] { fileNotFound };
+        }
 
-//            var service = new FileIntegration(requestMock);
+        //[TestMethod]
+        //[DynamicData(nameof(FileToSent))]
+        //public async Task Upload_File_Async_HttpException(string filePathValid, string receiver)
+        //{
+        //    // Arrange
+        //    var exception = new HttpRequestException("Falha na comunicação");
+        //    var requestMock = Helpes.Handlers.HttpHelperMock.CreateHttpClient
+        //        (out var handlerMock, ex: exception);
 
-//            // Act
-//            var expectedResult = await Assert.ThrowsAsync<ErrorLists>(async () =>
-//            {
-//                await service.UploadFileAsync(apiKey, filePathValid, receiver);
-//            });
+        //    var service = new FileIntegration(requestMock);
 
-//            // Assert
-//            expectedResult.ShouldBeOfType<ErrorLists>();
-//            expectedResult.Errors.ShouldContain($"Falha ao fazer upload do arquivo: {Path.GetFileName(filePathValid)}");
-//        }
+        //    // Act
+        //    var expectedResult = await Assert.ThrowsAsync<ErrorLists>(async () =>
+        //    {
+        //        await service.UploadFileAsync(apiKey, filePathValid, receiver);
+        //    });
 
-//        [TestMethod]
-//        [DynamicData(nameof(FileToSent))]
-//        public async Task Upload_File_Async_Sucess_Result(string filePathValid, string reciver)
-//        {
-//            // Arrange
-//            var expectedId = Guid.NewGuid();
+        //    // Assert
+        //    expectedResult.ShouldBeOfType<ErrorLists>();
+        //    expectedResult.Errors.ShouldContain($"Falha ao fazer upload do arquivo: {Path.GetFileName(filePathValid)}");
+        //}
 
-//            var response = new HttpResponseMessage(HttpStatusCode.OK)
-//            {
-//                Content = new StringContent("{\"id\":\"" + expectedId + "\"}", Encoding.UTF8, "application/json")
-//            };
-//            var responseMock = Helpes.Handlers.HttpHelperMock.CreateHttpClient
-//                (out var handlerMock, response: response);
+        //[TestMethod]
+        //[DynamicData(nameof(FileToSent))]
+        //public async Task Upload_File_Async_Sucess_Result(string filePathValid, string reciver)
+        //{
+        //    // Arrange
+        //    var expectedId = Guid.NewGuid();
 
-//            var service = new FileIntegration(responseMock);
+        //    var response = new HttpResponseMessage(HttpStatusCode.OK)
+        //    {
+        //        Content = new StringContent("{\"id\":\"" + expectedId + "\"}", Encoding.UTF8, "application/json")
+        //    };
+        //    var responseMock = Helpes.Handlers.HttpHelperMock.CreateHttpClient
+        //        (out var handlerMock, response: response);
 
-//            // Act
-//            var expectedResult = await service.UploadFileAsync(apiKey, filePathValid, reciver);
+        //    var service = new FileIntegration(responseMock);
 
-//            // Assert
-//            expectedResult.ShouldBeOfType<string>();
-//            expectedResult.ShouldBe(expectedId.ToString());
-//        }
+        //    // Act
+        //    var expectedResult = await service.UploadFileAsync(apiKey, filePathValid, reciver);
 
-//        public static IEnumerable<object[]> FileToSent()
-//        {
-//            var filePathValid = "C:\\Users\\Ajinsoft\\Pictures\\imgs\\identity_test.jpg";
-//            var receiver = _faker.Person.FirstName;
+        //    // Assert
+        //    expectedResult.ShouldBeOfType<string>();
+        //    expectedResult.ShouldBe(expectedId.ToString());
+        //}
 
-//            yield return new object[] { filePathValid, receiver };
-//        }
-//    }
-//}
+        //public static IEnumerable<object[]> FileToSent()
+        //{
+        //    var filePathValid = "C:\\Users\\Ajinsoft\\Pictures\\imgs\\identity_test.jpg";
+        //    var receiver = _faker.Person.FirstName;
+
+        //    yield return new object[] { filePathValid, receiver };
+        //}
+    }
+}
